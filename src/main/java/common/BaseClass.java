fd981c2022c3378b30fa1fc62f682d89ce558e41
Amit Kumar2 <amitk2@chetu.com>
2022-05-09 18:41:53 +0530

    initializing selenium test project

---
+++ src/main/java/common/BaseClass.java
@@ -0,0 +1,29 @@
+package common;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+import java.util.Optional;
+
+public class BaseClass {
+
+    public static void main(String... args){
+        openBrowser();
+    }
+    public static void openBrowser(){
+        System.setProperty(
+                "webdriver.chrome.driver",
+                "E:\\amit\\Java_programs\\TestingSelenium\\src\\main\\resources\\chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--auto-open-devtools-for-tabs");
+        WebDriver driver = new ChromeDriver(options);
+
+        // Launch Website
+        driver.get("https://www.amazon.in/");
+        System.out.println(driver.getTitle());
+    }
+}
---
+++ src/test/java/selenium/test/SpiritAirlines.java
@@ -0,0 +1,4 @@
+package selenium.test;
+
+public class SpiritAirlines {
+}
